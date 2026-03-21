1fa3cdd6507dec9dfa820aa633ed87493382e7e5
adelnizamutdinov <stiggpwnz@gmail.com>
2026-03-21 14:26:13 +0300

    amen

--- src/dsl/dsl.ts
+++ src/dsl/dsl.ts
@@ -2,7 +2,7 @@ import type { oas31 } from "openapi3-ts"
 import type { Routes, ScopeOpts } from "./scope.ts"
 
 /**
- * this this core of the DSL. Named components are intentionally banned,
+ * this is core of the DSL. Named components are intentionally banned,
  * we use {@link import("./nameable.ts").Nameable}
  */
 type BanComponents = Omit<oas31.OpenAPIObject, "components">
