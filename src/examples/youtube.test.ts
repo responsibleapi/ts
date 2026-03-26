b0d166f4b8069c20a2480a9071b7abad086dab19
adelnizamutdinov <stiggpwnz@gmail.com>
2026-03-26 11:55:14 +0300

    amen

--- src/examples/youtube.test.ts
+++ src/examples/youtube.test.ts
@@ -8,7 +8,9 @@ describe("youtube example", () => {
   })
 
   test.skip("youtube.json validates as OpenAPI", async () => {
+    /** compiler isn't implemented, this import throws */
     const { default: api } = await import("./youtube.ts")
+
     expect(await validate(api)).toEqual<typeof json>(json)
   })
 })
