643256ea8ed4e36733cbe216faedf27bbe5aa4e0
adelnizamutdinov <stiggpwnz@gmail.com>
2026-04-01 03:04:42 +0300

    amen

--- src/examples/listenbox.test.ts
+++ src/examples/listenbox.test.ts
@@ -2,9 +2,9 @@ import { describe, expect, test } from "vitest"
 import { validate } from "../validate.ts"
 import json from "./listenbox.json"
 
-describe("listenbox example", () => {
+describe("listenbox", () => {
   test("listenbox.json validates as OpenAPI", async () => {
-    /** compiler isn't implemented, this import throws */
+    /** Compiler isn't implemented, this import throws */
     const { default: api } = await import("./listenbox.ts")
 
     expect(await validate(api)).toEqual<typeof json>(json)
